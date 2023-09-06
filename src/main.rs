7214da458eeb5b3c824c9ae95dfe0ce93ecfeeec
Jim Raden <radenj@ccf.org>
2023-09-06 19:11:24 -0400

    Kim's first attempt. BROKEN

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let a = 38;
+    let b = 4;
+    println!("{} + {} = {}", a, b, add(a, b));
+}
+
+fn add(n1: i32, n2: i32) -> i32 {
+    let mut sum = n1;
+    let (count, increment) = if n2 > 0 { (n2, 1) } else { (-n2, -1) };
+    let mut handles = vec![];
+
+    for _ in 0..count {
+        handles.push(thread::spawn(|| {
+            sum += increment;
+        }));
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    sum
 }
