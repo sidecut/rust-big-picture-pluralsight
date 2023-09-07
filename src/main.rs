ee59af2d9b4cc285b2fc3f922d6f17d024c82b54
James Raden <james.raden@gmail.com>
2023-09-06 21:40:19 -0400

    Remove accumulator; use main.rs from 7214da4 (Kim's first attempt. BROKEN, 2023-09-06)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
-mod accumulator;
-
-use std::sync::{Arc, Mutex};
 use std::thread;
-
-use accumulator::Accumulator;
 
 fn main() {
     let a = 38;
@@ -12,15 +7,13 @@ fn main() {
 }
 
 fn add(n1: i32, n2: i32) -> i32 {
-    let acc = Arc::new(Mutex::new(Accumulator::new(n1)));
+    let mut sum = n1;
     let (count, increment) = if n2 > 0 { (n2, 1) } else { (-n2, -1) };
     let mut handles = vec![];
 
     for _ in 0..count {
-        let guarded_acc = Arc::clone(&acc);
-        handles.push(thread::spawn(move || {
-            let mut guarded_acc = guarded_acc.lock().unwrap();
-            guarded_acc.add(increment);
+        handles.push(thread::spawn(|| {
+            sum += increment;
         }));
     }
 
@@ -28,6 +21,5 @@ fn add(n1: i32, n2: i32) -> i32 {
         handle.join().unwrap();
     }
 
-    let final_acc = acc.lock().unwrap();
-    final_acc.get_sum()
+    sum
 }
