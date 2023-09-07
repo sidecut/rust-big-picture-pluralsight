d0abbb5d3777df491a998b15b2c5286c6e3f533b
James Raden <james.raden@gmail.com>
2023-09-06 21:47:51 -0400

    Use producer/consumer model using channels

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::thread;
+use std::{sync::mpsc, thread};
 
 fn main() {
     let a = 38;
@@ -10,15 +10,21 @@ fn add(n1: i32, n2: i32) -> i32 {
     let mut sum = n1;
     let (count, increment) = if n2 > 0 { (n2, 1) } else { (-n2, -1) };
     let mut handles = vec![];
+    let (tx, rx) = mpsc::channel();
 
     for _ in 0..count {
-        handles.push(thread::spawn(|| {
-            sum += increment;
+        let tx_for_thread = tx.clone();
+        handles.push(thread::spawn(move || {
+            tx_for_thread.send(increment).unwrap()
         }));
     }
 
     for handle in handles {
         handle.join().unwrap();
+    }
+
+    for _ in 0..count {
+        sum += rx.recv().unwrap();
     }
 
     sum
